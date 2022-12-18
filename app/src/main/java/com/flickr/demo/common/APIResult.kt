ede1f2da6c874a0a6d760e12de06f0d62bca881f
ashatmishra <ashatmishra@users.noreply.github.com>
2022-12-19 01:11:43 +0800

    [Flickr Findr] - Removed unused file

--- app/src/main/java/com/flickr/demo/common/APIResult.kt
+++
@@ -1,7 +0,0 @@
-package com.flickr.demo.common
-
-sealed class APIResult<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T): APIResult<T>(data)
-    class Error<T>(data: T? = null, message: String): APIResult<T>(data, message)
-    class Loading<T>(data: T? = null): APIResult<T>(data)
-}
